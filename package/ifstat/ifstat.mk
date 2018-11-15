7a34072052584915153a2e39e4878fb4900d1c6f
jgsun <jianguo_sun@hotmail.com>
2018-11-15 16:34:50 +0800

    package: ifstat: remove comments
    Remove unusful comments.

--- package/ifstat/ifstat.mk
+++ package/ifstat/ifstat.mk
@@ -6,15 +6,8 @@
 
 IFSTAT_VERSION = 1.1
 IFSTAT_SITE = http://gael.roualland.free.fr/ifstat
-# IFSTAT_DEPENDENCIES = ncurses libpcap
 IFSTAT_LICENSE = GPL-2.0+
 IFSTAT_LICENSE_FILES = COPYING
-
-# IFSTAT_LIBS = -lpcap
-# ifeq ($(BR2_STATIC_LIBS),y)
-# IFSTAT_LIBS += `$(STAGING_DIR)/usr/bin/pcap-config --static --additional-libs`
-# endif
-# IFSTAT_CONF_ENV += LIBS+="$(IFSTAT_LIBS)"
 
 define IFSTAT_CONFIGURE_CMDS
 	cd $(@D) && $(TARGET_CONFIGURE_OPTS) ./configure
@@ -25,7 +18,6 @@ define IFSTAT_BUILD_CMD
 endef
 
 define IFSTAT_INSTALL_TARGET_CMDS
-#	$(TARGET_MAKE_ENV) DESTDIR="$(TARGET_DIR)" $(MAKE) -C $(@D) install
 	$(INSTALL) -m 0755 -D $(@D)/ifstat $(TARGET_DIR)/usr/bin/if-stat
 endef
 
