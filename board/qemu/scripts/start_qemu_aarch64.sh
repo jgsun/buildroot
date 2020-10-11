72a122384ac0f09f00d427fe99ae97edd55b458b
jgsun <jianguo_sun@hotmail.com>
2020-10-11 20:04:35 +0800

    board: qemu: update start_qemu_aarch64.sh
    To use relative directory, so we can run it in buildroot top directory.
    Make sure let qemu-system-aarch64 in system PATH.

--- board/qemu/scripts/start_qemu_aarch64.sh
+++ board/qemu/scripts/start_qemu_aarch64.sh
@@ -1,8 +1,8 @@
-sudo ~/repo/qemu/aarch64-softmmu/qemu-system-aarch64 -M virt \
+sudo qemu-system-aarch64 -M virt \
 	-cpu cortex-a57 -nographic -smp 4 -m 512 \
-	-kernel ~/repo/buildroot-arm64/output/images/Image \
+	-kernel output/images/Image \
 	-append "root=/dev/ram0 console=ttyAMA0 kmemleak=on loglevel=8" \
-	-netdev type=tap,ifname=tap0,id=eth0,script=/home/jgsun/repo/buildroot-arm64/board/qemu/scripts/qemu-ifup_virbr0,queues=2 \
+	-netdev type=tap,ifname=tap0,id=eth0,script=board/qemu/scripts/qemu-ifup_virbr0,queues=2 \
 	-device virtio-net-pci,netdev=eth0,mac='00:00:00:01:00:01',vectors=6,mq=on \
 #	dyndbg=\"file drivers/of/irq.c +p\"" \
 #	-netdev type=tap,ifname=tap1,id=eth1,script=~/repo/buildroot/board/qemu/scripts/qemu-ifup_virbr0,queues=2 \
