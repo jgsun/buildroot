7f28ea78955970f63ceff6d791087efaa3dff433
Jianguo Sun <jianguo_sun@hotmail.com>
2022-07-17 17:10:13 +0800

    update for rebase 2022.02.x

--- package/ply/ply.mk
+++ package/ply/ply.mk
@@ -4,15 +4,7 @@
 #
 ################################################################################
 
-<<<<<<< HEAD
 PLY_VERSION = 2.2.0
-=======
-PLY_VERSION = 2.1.1
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> pakage: add target package ply
-=======
->>>>>>> rebase to 2021.02.x
 PLY_SITE = $(call github,wkz,ply,$(PLY_VERSION))
 PLY_AUTORECONF = YES
 PLY_LICENSE = GPL-2.0
